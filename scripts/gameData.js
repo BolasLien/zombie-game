5455b7a7ef2bda7cbe74f11751c430a79da162ba
BolasLien <ltzuen@gmai.com>
2020-04-20 14:38:21 +0800

    怪物資料重構

---
+++ scripts/gameData.js
@@ -0,0 +1,63 @@
+const itemData = [
+    {
+        "name": "boomer",
+        "srcNormal": "./boomer/boomer.gif",
+        "srcAtk": "./boomer/boomer-atk.gif",
+        "atk": 1,
+        "score": 2
+    },
+
+    {
+        "name": "smoke",
+        "srcNormal": "./smoke/smoke.gif",
+        "srcAtk": "./smoke/smoke-atk.gif",
+        "atk": 1,
+        "score": 3
+    },
+
+    {
+        "name": "chager",
+        "srcNormal": "./chager/chager.gif",
+        "srcAtk": "./chager/chager-atk.gif",
+        "atk": 2,
+        "score": 5
+    },
+
+    {
+        "name": "witch",
+        "srcNormal": "./witch/witch.gif",
+        "srcAtk": "./witch/witch-atk.gif",
+        "atk": 1,
+        "score": 2
+    },
+
+    {
+        "name": "tank",
+        "srcNormal": "./tank/tank.gif",
+        "srcAtk": "./tank/tank-atk.gif",
+        "srcDead": "./tank/tank-dead.gif",
+        "atk": 3,
+        "score": 10
+    },
+
+    {
+        "name": "tank",
+        "srcNormal": "./tank/tank-run.gif",
+        "srcAtk": "./tank/tank-atk2.gif",
+        "srcDead": "./tank/tank-dead.gif",
+        "atk": 3,
+        "score": 10
+    },
+    {
+        "name": "life",
+        "srcNormal": "./life.png",
+        "life": 1
+    }
+
+]
+
+const levelData = [
+    [0, 100, 100, 100, 0, 0, 20],
+    [100, 0, 100, 0, 0, 0, 20],
+    [0, 0, 0, 0, 100, 0, 20]
+]
